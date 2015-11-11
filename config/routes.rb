00aa67f64abe601b7f7d2fce44877d0cc683f19e
Tacksoo Im <tacksoo.im@gmail.com>
2015-11-11 04:01:38 +0000

    chapter 7 rails tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'about' => 'home#about'
   get 'contact' => 'home#contact'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

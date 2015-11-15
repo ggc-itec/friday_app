858024eed19c492a96b87ed1b6af1f547c3f3fd5
Tacksoo Im <tacksoo.im@gmail.com>
2015-11-15 15:48:04 +0000

    enabled ssl

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @word = "This is the WORD!"
     @word = add_p(@word)
+    @user = User.find(1)
   end
 
   def about
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])  
     #render :show
   end
+  
+  def new
+    @user = User.new 
+  end
+  
+  def create
+    @user = User.new(user_params) 
+    if @user
+      return "User added successfully" 
+    else
+      return "User not added successfully" 
+    end 
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+  
 end

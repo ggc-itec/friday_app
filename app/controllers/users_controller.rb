75a297289cca964213152625d02bd253884362c7
Tacksoo Im <tacksoo.im@gmail.com>
2015-11-16 05:06:42 +0000

    added index for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])  
-    #render :show
   end
   
   def new
@@ -10,18 +9,19 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params) 
-    if @user
-      return "User added successfully" 
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      return "User not added successfully" 
+      render 'new'
     end 
   end
   
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
   
 end
